69abf5a2552d29a5e0d8ae44ca6b196b9f81f6d3
jonyf <jonyf@FIGHT>
2025-04-16 13:15:20 +0200

    Přídání pozdravu

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Zadej svoje jméno!");
+string jmeno = Console.ReadLine();
+Console.WriteLine("Ahoj {0}",jmeno);
